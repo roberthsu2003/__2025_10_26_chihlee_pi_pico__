86870c3c6c8a13af0ff5c35e3656cccee92eab70
徐國堂 <roberthsu2003@gmail.com>
2025-11-23 11:47:03 +0800

    Update lesson5_1.py

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -1,3 +1,19 @@
 from machine import Timer
 
-timer = Timer(period=2000, mode=Timer.PERIODIC, callback=lambda t:print('Hello! Pico'))
+count = 0
+
+def callback2000(n):    
+    global count
+    count += 1
+    if count == 5:
+        n.deinit()
+        print("再見")
+    else:
+        print("Hello! Pico")
+       
+    
+def main():
+    timer = Timer(period=2000, callback=callback2000)
+    
+if __name__ == "__main__":
+    main()
