55e203fbbe78aad9c6627799890f0c0c8973d3cc
徐國堂 <roberthsu2003@gmail.com>
2025-12-07 13:45:58 +0800

    modify

--- lesson7/main.py
+++ lesson7/main.py
@@ -1,13 +1,13 @@
-import wifi_connect
+import wifi_connect as wifi
 
 # 嘗試連線 WiFi
-wifi_connect.connect()
+wifi.connect()
 
 # 顯示 IP
-print("IP:", wifi_connect.get_ip())
+print("IP:", wifi.get_ip())
 
 # 測試外部網路
-if wifi_connect.test_internet():
+if wifi.test_internet():
     print("外部網路 OK")
 else:
     print("外部網路無法連線")
