1f52fd565cf54dc89ed528630a6f05014e37b7aa
徐國堂 <roberthsu2003@gmail.com>
2025-12-07 14:15:46 +0800

    modify

--- lesson7/main.py
+++ lesson7/main.py
@@ -1,4 +1,5 @@
 import wifi_connect as wifi
+import time
 
 # 嘗試連線 WiFi
 wifi.connect()
@@ -6,8 +7,13 @@ wifi.connect()
 # 顯示 IP
 print("IP:", wifi.get_ip())
 
-# 測試外部網路
-if wifi.test_internet():
-    print("外部網路 OK")
-else:
-    print("外部網路無法連線")
+# 每隔 10 秒測試一次外部網路
+while True:
+    print("-" * 30)
+    if wifi.test_internet():
+        print("外部網路 OK")
+    else:
+        print("外部網路無法連線")
+    
+    print("等待 10 秒後再次測試...")
+    time.sleep(10)
