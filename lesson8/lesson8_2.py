c5900fbc94f051c3e2b65824604d94cc32173088
徐國堂 <roberthsu2003@gmail.com>
2025-12-14 14:42:57 +0800

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -2,14 +2,15 @@ from machine import Pin
 from time import sleep
 
 btn_pin = 14
+led_pin = 15
 
 button = Pin(btn_pin,Pin.IN,Pin.PULL_UP)
+led = Pin(led_pin, Pin.OUT)
 
 while(True):
     if button.value() == 1:
-        print("沒按")
+        led.off()
     else:
-        print("按下")
-    sleep(1)
+        led.on()
     
     
